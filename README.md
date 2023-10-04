9a7181c90bcbab749342d843b9b7d8ab590bc7e6
Alexsandr Matsveyeu <alexsandr.matsveyeu@refinitiv.com>
2023-10-04 23:52:28 +0200

    finish BE on task-3

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# swagger file
+
+[swagger](swagger.yaml)
+
+# url with source:
+
+* https://www.honga.net/totalwar/warhammer2/unit.php?l=en&v=warhammer2&f=wh2_main_def_dark_elves_qb2
---
+++ product-service/functions/getProductsList.js
@@ -0,0 +1,8 @@
+export default async function (event) {
+  const products = await import("../mocks/products.json");
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products)
+  };
+}
